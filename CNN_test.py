693a1e15bdbd309a562db741c3f914ffd1a1992a
TaikiSakai <s.taiki0301@gmail.com>
2023-07-29 00:38:59 +0900

    update

--- CNN_test.py
+++ CNN_test.py
@@ -26,6 +26,7 @@ for i in range(25):
 plt.show()
 
 model = models.Sequential()
+#以下に処理を記述する
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
